81596c36480dd9010abff42b64cc97b786201658
sabirhassan <l164258@lhr.nu.edu.pk>
2020-10-20 18:42:20 +0500

    added relationships between models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ApplicationRecord
+
+  has_many :product_categories
+  has_many :products, through: :product_categories
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  validates :title, presence: true
+
+  has_many :product_categories
+  has_many :categories, through: :product_categories
 end
