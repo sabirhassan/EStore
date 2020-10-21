c3aade51da2d7d0fba2c4f13894d067e71cd9279
sabirhassan <l164258@lhr.nu.edu.pk>
2020-10-21 10:52:10 +0500

    added products controller

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,7 @@
+class ProductsController < ApplicationController
+  
+  def index
+    @category = Category.find(params[:category_id])
+    @products = @category.products.includes(:variants).order(:title)
+  end
+end
