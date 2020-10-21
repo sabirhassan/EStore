a45052c4cc8ba15f98f67df31913567451ccf6e2
sabirhassan <l164258@lhr.nu.edu.pk>
2020-10-21 15:46:31 +0500

    sub_total functionality implemented

--- app/models/shopping_cart.rb
+++ app/models/shopping_cart.rb
@@ -1,5 +1,7 @@
 class ShoppingCart
 
+  delegate :sub_total, to: :order
+
   def initialize(token:)
     @token = token
   end
@@ -24,11 +26,23 @@ class ShoppingCart
     order_item.price = product.price
     order_item.quantity = quantity
 
-    order_item.save
+    ActiveRecord::Base.transaction do
+      order_item.save
+      update_sub_total!
+    end
   end
 
   def remove_item(id:)
-    order.items.destroy(id)
+    ActiveRecord::Base.transaction do
+      order.items.destroy(id)
+      update_sub_total!
+    end
+  end
+
+  private
+  def update_sub_total!
+    order.sub_total = order.items.sum('quantity*price')
+    order.save
   end
 
 end
